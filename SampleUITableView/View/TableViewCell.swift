ab66caa0694666acf3a964d1d4572d4e93ba734c
sakiyamaK <sakiyama.k@gmail.com>
2020-11-04 21:55:57 +0900

    a

--- SampleUITableView/View/TableViewCell.swift
+++ SampleUITableView/View/TableViewCell.swift
@@ -9,30 +9,38 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-    @IBOutlet private weak var iconView: UIImageView!
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var mainTextLabel: UILabel!
-    @IBOutlet private weak var contentsImageView: UIImageView!
+  @IBOutlet private weak var iconView: UIImageView!
+  @IBOutlet private weak var nameLabel: UILabel!
+  @IBOutlet private weak var mainTextLabel: UILabel!
+  @IBOutlet private weak var contentsImageView: UIImageView!
 
-    func configure(user: UserModel) {
-        //本当はapi通信してアイコン画像を読み込む
-        if let iconImageUrlStr = user.icon {
-	        //iconView = ~~~~
-        }
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    iconView.image = nil
+    nameLabel.text = nil
+    mainTextLabel.text = nil
+    contentsImageView.image = nil
+  }
 
-        nameLabel.text = user.name
-        mainTextLabel.text = user.mainText
-
-        //本当はapi通信してコンテンツの画像を読み込む
-        if let cotentImageURLStr = user.image {
-            //contentsImageView
-        } else {
-            contentsImageView.isHidden = true
-        }
+  func configure(user: UserModel) {
+    //本当はapi通信してアイコン画像を読み込む
+    if let iconImageUrlStr = user.icon {
+      //iconView = ~~~~
     }
 
-    static func cellHeight(user: UserModel) -> CGFloat {
-        /*読み込むデータに対して高さだけ決める*/
-        return 1000
+    nameLabel.text = user.name
+    mainTextLabel.text = user.mainText
+
+    //本当はapi通信してコンテンツの画像を読み込む
+    if let cotentImageURLStr = user.image {
+      //contentsImageView
+    } else {
+      contentsImageView.isHidden = true
     }
+  }
+
+  static func cellHeight(user: UserModel) -> CGFloat {
+    /*読み込むデータに対して高さだけ決める*/
+    return 1000
+  }
 }
--- SampleUITableView/ViewController/TableViewController.swift
+++ SampleUITableView/ViewController/TableViewController.swift
@@ -20,100 +20,102 @@ import UIKit
 
 class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    private let cellClassName = "TableViewCell"
-    private let reuseId = "TableViewCell"
+  private let cellClassName = "TableViewCell"
+  private let reuseId = "TableViewCell"
 
-    private let api = SampleAPI()
-    private var users:[UserModel] = []
+  private let api = SampleAPI()
+  private var users:[UserModel] = []
+  private var heightCache: [CGFloat] = []
 
-    @IBOutlet weak var indicator: UIActivityIndicatorView!
-    @IBOutlet weak var tableView: UITableView! {
-        didSet {
-            /* セルの登録
-             読み込ませたいセルの種類の数だけxibを用意してそれを登録するのが一般的
-             xibを使わない場合などは登録方法が違う
-             今時は便利なライブラリを使うがここではiOS標準機能だけで書いている
-             */
-            //xibファイルを読み込む
-            let cellNib = UINib(nibName: cellClassName, bundle: nil)
-            //このtableViewはこのセルを使いますよと登録する
-            tableView.register(cellNib, forCellReuseIdentifier: reuseId)
-            
-            /*
-             dataSourceとdelegateをどのViewControllerに任せるか
-             普通はself
-             autolayoutからも設定ができる
-             */
-//            tableView.dataSource = self
-//            tableView.delegate = self
+  @IBOutlet weak var indicator: UIActivityIndicatorView!
+  @IBOutlet weak var tableView: UITableView! {
+    didSet {
+      /* セルの登録
+       読み込ませたいセルの種類の数だけxibを用意してそれを登録するのが一般的
+       xibを使わない場合などは登録方法が違う
+       今時は便利なライブラリを使うがここではiOS標準機能だけで書いている
+       */
+      //xibファイルを読み込む
+      let cellNib = UINib(nibName: cellClassName, bundle: nil)
+      //このtableViewはこのセルを使いますよと登録する
+      tableView.register(cellNib, forCellReuseIdentifier: reuseId)
 
+      /*
+       dataSourceとdelegateをどのViewControllerに任せるか
+       普通はself
+       autolayoutからも設定ができる
+       */
+//      tableView.dataSource = self
+//      tableView.delegate = self
 
-            /*
-             推定の高さ
-             固定の高さ
+      /*
+       推定の高さ
+       固定の高さ
 
-             tableViewはデフォルトでこのあたりを設定している
-             autolayoutからも設定ができる
+       tableViewはデフォルトでこのあたりを設定している
+       autolayoutからも設定ができる
 
-             細かく指定した方が画面はスムーズに動く
-             */
-//            tableView.estimatedRowHeight = 120
-//            tableView.rowHeight = 120
-        }
+       細かく指定した方が画面はスムーズに動く
+       */
+//      tableView.estimatedRowHeight = 120
+//      tableView.rowHeight = 120
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  }
 
-        //api通信してテーブルをリロード
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        api.getHome { (users, error) in
-            if let _error = error {
-                //api通信エラーが起きた時の処理
-                debugPrint(_error)
-                return
-            }
-            guard let _users = users else {
-                //apiは成功してるのにレスポンスが空の時
-                return
-            }
-            //成功してたら代入してリロード
-            self.users = _users
-            self.tableView.isHidden = false
-            self.indicator.isHidden = true
-            self.tableView.reloadData()
-        }
+    //api通信してテーブルをリロード
+
+    api.getHome { (users, error) in
+      if let _error = error {
+        //api通信エラーが起きた時の処理
+        debugPrint(_error)
+        return
+      }
+      guard let _users = users else {
+        //apiは成功してるのにレスポンスが空の時
+        return
+      }
+      //成功してたら代入してリロード
+      self.users = _users
+      self.tableView.isHidden = false
+      self.indicator.isHidden = true
+      self.tableView.reloadData()
     }
+  }
 
 
-    /*
-     絶対に実装を追加しないといけないのはこのふたつ
-     残りのセクション数などはデフォルトになっている
-     */
+  /*
+   絶対に実装を追加しないといけないのはこのふたつ
+   残りのセクション数などはデフォルトになっている
+   */
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return users.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath) as? TableViewCell else {
+      return UITableViewCell()
     }
+    let user = users[indexPath.row]
+    cell.configure(user: user)
+    return cell
+  }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseId, for: indexPath) as? TableViewCell else {
-            return UITableViewCell()
-        }
-        let user = users[indexPath.row]
-        cell.configure(user: user)
-        return cell
-    }
-
-    /*
-     セルごとに高さを変える場合はこのfunctionを実装して計算する
-     他に楽なやり方もある
-     だけどどうしてもパフォーマンスチューニングをする必要がある時はここをうまくやる
-     */
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        /*ガリガリ計算してセルの高さ*/
-//        let user = users[indexPath.row]
-//        let height = TableViewCell.cellHeight(user: user)
-//        return height
-//    }
+  /*
+   セルごとに高さを変える場合はこのfunctionを実装して計算する
+   他に楽なやり方もある
+   だけどどうしてもパフォーマンスチューニングをする必要がある時はここをうまくやる
+   */
+//  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//    /*ガリガリ計算してセルの高さ*/
+//    let user = users[indexPath.row]
+//    let height = TableViewCell.cellHeight(user: user)
+////    heightCache[indexPath.row] = height
+//
+//    return height
+//  }
 }
 
